4be3f626e92b683df8ddb0685e7d0306ce1ee1d8
Nicholas Kumia <nicholas.kumia@reisystems.com>
2023-03-10 09:49:23 -0500

    fix: remove bad indent from policy
    https://stackoverflow.com/a/42670283

--- terraform/lambda/provision/lambda.tf
+++ terraform/lambda/provision/lambda.tf
@@ -3,19 +3,19 @@ resource "aws_iam_role" "iam_for_lambda" {
   name = "iam_for_solr_${var.name}_restarts"
 
   assume_role_policy = <<-EOF
-  {
-    "Version": "2012-10-17",
-    "Statement": [
-      {
-        "Action": "sts:AssumeRole",
-        "Principal": {
-          "Service": "lambda.amazonaws.com"
-        },
-        "Effect": "Allow"
-      }
-    ]
-  }
-  EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
 }
 
 resource "aws_iam_role_policy_attachment" "lambda_operations" {
